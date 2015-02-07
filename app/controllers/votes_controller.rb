920fe5fc557d3e2f24c247e8d0262872f45dcd9d
Jacob Barrieault <jacob@quikly.com>
2015-02-07 10:14:04 -0500

    prevent self-voting

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,7 @@ class VotesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @users = User.where.not(id: current_user.id)
   end
 
 	def create
