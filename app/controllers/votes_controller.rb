17c4749cf0f820a601034a0fae3034060458a05a
Jacob Barrieault <jacob@quikly.com>
2015-02-10 23:46:46 -0500

    forgot to uncomment authenticate_user

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,5 @@
 class VotesController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     @users = User.where.not(id: current_user.id)
